6fe05fe3c740f2b5896dfa877306425cd863ed24
Love Almgren <lovealmgren@Loves-MacBook-Pro.local>
2017-05-05 11:12:52 +0200

    fixed mvn eclipse issue

--- src/main/java/com/enigio/tbf/VocoStart.java
+++ src/main/java/com/enigio/tbf/VocoStart.java
@@ -1,9 +1,22 @@
+package com.enigio.tbf;
 
-package com.enigio.voco;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class VocoStart {
 	
 	public static String VOCO = "";
 	public static String TEST = "";
-	
+
+	HttpResponse<JsonNode> jsonResponse = Unirest.post("http://httpbin.org/post")
+			.header("accept", "application/json")
+			.queryString("apiKey", "123")
+			.field("parameter", "value")
+			.field("foo", "bar")
+			.asJson();
+
+	public VocoStart() throws UnirestException {
+	}
 }
